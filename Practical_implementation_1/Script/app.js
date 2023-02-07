7619ac09be0a11397da098d5fecedb5cc7a19d0e
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-02-07 21:15:26 +0400

    update

--- Practical_implementation_1/Script/app.js
+++ Practical_implementation_1/Script/app.js
@@ -2,5 +2,12 @@
 
 let nameUser = prompt("Hello, what's your name?", "");
 
-
 alert(`Hello ${nameUser}, good afternoon.`);
+
+let ageUser = prompt("How old are you?", "");
+
+if (ageUser >= 18) {
+    alert(`${nameUser} welcome to the website!`);
+} else {
+    alert("You are denied access!")
+}
