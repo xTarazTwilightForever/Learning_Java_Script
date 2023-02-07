ebb48919678bf1bf1e61ff3824f14c77a799d537
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-02-08 03:55:08 +0400

    Added checks, loops and conditions.

--- Scripts/Saving_Information_User.js
+++ Scripts/Saving_Information_User.js
@@ -1,18 +1,43 @@
 "use strict";
-function saving_Information_User(){
-let numberOfFilms = prompt("How many movies have you watched?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+function saving_Information_User() {
+    let userWatchMovies = true;
+    let numberOfFilms = prompt("How many movies have you watched?", "");
 
-let lastUserMovie = prompt("One of the last watched movies?", ""),
-    filmUserScore = prompt("How much do you rate it?", "");
-personalMovieDB.movies[lastUserMovie] = filmUserScore;
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private: false
+    };
 
-console.log(personalMovieDB);
+    if (personalMovieDB.count == 0) {
+        userWatchMovies = false;
+        console.log("You don't watch movies. ");
+    } else if (personalMovieDB.count < 10) {
+        console.log("Seen quite a few movies.");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log("You are a great viewer!");
+    } else if (personalMovieDB.count > 30) {
+        console.log("You're a movie buff!")
+    } else {
+        console.log("Error.")
+    }
+    if (userWatchMovies == true) {
+        for (let i = 0; i < 2; i++) {
+
+            const lastUserMovie = prompt("One of the last watched movies?", ""),
+                filmUserScore = prompt("How much do you rate it?", "");
+
+            if (lastUserMovie != null && filmUserScore != null && lastUserMovie != '' && filmUserScore != '' && lastUserMovie.length < 50) {
+                personalMovieDB.movies[lastUserMovie] = filmUserScore;
+                console.log(personalMovieDB);
+                console.log("Done!");
+            } else {
+                console.log("Eror!");
+                i--;
+            }
+        }
+    }
 }
