40184b15efaa1cf3bff7ad4c1594c448ff54f462
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-03-06 21:02:01 +0400

    New structur

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,4 +4,5 @@ function listingLink(num,clr,book){
 }
 
 let buttonСolors = ["#FF3131", "#FFF01F", "#FF44CC", "#F1FF30", "#ECF0F1", "#9B59B6", "#8E44AD", "#2ECC71", "#1ABC9C", "#3498DB", "#F39C12", "39FF14", "FFF01F", "EA00FF"];
-var buttonСolors2 = ["#BC13FE", "#7FFF00", "#FFFFFF", "#FFF01F", "#7FFF00", "#E7324F", "#FFF01F", "#FF3131", "#8A2BE2", "#FFFFFF", "#E7324F"];
+let buttonСolors2 = ["#BC13FE", "#7FFF00", "#FFFFFF", "#FFF01F", "#7FFF00", "#E7324F", "#FFF01F", "#FF3131", "#8A2BE2", "#FFFFFF", "#E7324F", "#FF44CC", "#F1FF30", "#ECF0F1", "#9B59B6"];
+var buttonСolors3 = ["#141226", "#87a3a0", "#c66d94", "#1436bc", "#7FFF00", "#ed32ff", "#4f0f2a", "#211a16", "#938b3f", "#47cc0e", "#8c7b7f", "#8645e8", "#b7bca7", "#7FFF00","#BC13FE", "#7FFF00", "#FFFFFF", "#FFF01F", "#7FFF00", "#E7324F", "#FFF01F", "#FF3131", "#8A2BE2", "#FFFFFF", "#E7324F", "#FF44CC", "#F1FF30", "#ECF0F1", "#9B59B6"];
