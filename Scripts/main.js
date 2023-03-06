b9d609b4cb4e2723fe741d452d97b308781f5e09
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-03-06 20:38:14 +0400

    New button system

---
+++ Scripts/main.js
@@ -0,0 +1,7 @@
+function listingLink(num,clr,book){
+    return `<a href="Book_Studies/${book}/Listing ${num}/index.html">
+				<button style="--clr:${clr}"><span>Listing ${num}</span><i></i></button></a>`;
+}
+
+let buttonСolors = ["#FF3131", "#FFF01F", "#FF44CC", "#F1FF30", "#ECF0F1", "#9B59B6", "#8E44AD", "#2ECC71", "#1ABC9C", "#3498DB", "#F39C12", "39FF14", "FFF01F", "EA00FF"];
+var buttonСolors2 = ["#BC13FE", "#7FFF00", "#FFFFFF", "#FFF01F", "#7FFF00", "#E7324F", "#FFF01F", "#FF3131", "#8A2BE2", "#FFFFFF", "#E7324F"];
