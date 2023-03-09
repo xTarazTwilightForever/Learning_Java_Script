fa6cd235a32477a90b22c2d10f8317737ae72edb
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-03-09 09:23:26 +0400

    HelloSpookyApp

--- Scripts/Display_Tables.js
+++ Scripts/Display_Tables.js
@@ -76,7 +76,7 @@
 		},
 		{
 			title: "Learning React: A Hands-On Guide to Building Web Applications Using React and Redux",
-			whereNow: 208,
+			whereNow: 220,
 			howMany: 365,
 			progress: "In progress..."
 		},
