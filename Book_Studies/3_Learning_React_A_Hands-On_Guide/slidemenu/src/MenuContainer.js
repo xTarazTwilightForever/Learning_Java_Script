5c461f0594f68f28dffb537e1223e14de4ca97dc
xTarazTwilightForever <xtaraztwilightforever@gmail.com>
2023-03-14 05:04:53 +0600

    Creating an application template

---
+++ Book_Studies/3_Learning_React_A_Hands-On_Guide/slidemenu/src/MenuContainer.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+
+class MenuContainer extends Component {
+    constructor (props) {
+        super(props);
+    
+        this.state = {
+            visible: false
+        };
+
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    handleMouseDown(e) {
+        this.toggleMenu();
+
+        console.log("clicked");
+        e.stopPropagation();
+    }
+
+    toggleMenu() {
+        this.setState({
+            visible:!this.state.visible
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>Find the item that is redundant here:</p>
+                    <ul>
+                        <li>Twilight</li>
+                        <li>Spooky</li>
+                        <li>AppleJack</li>
+                        <li>Fluttershy</li>
+                        <li>Pinkie Pai</li>
+                        <li>Rainbow Dash</li>
+                        <li>Rarity</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MenuContainer;
